991307c9ecfd494b8206930ddcf1ca1230f5124f
RafaelB411 <rafaelb411.rb@gmail.com>
2023-11-22 15:45:12 -0300

    question 5 done

--- lista-1/calculadora.hs
+++ lista-1/calculadora.hs
@@ -1,21 +1,28 @@
 type Comando = String
 type Valor = Int
 
+invertPairs :: [(Comando,Valor)] -> [(Comando,Valor)]
+invertPairs [] = []
+invertPairs (pair:pairs) = invertPairs pairs ++ [pair]
+
+operador :: [(Comando,Valor)] -> Int
+operador [(cmd,num)] = case cmd of
+                       "Soma" -> num
+                       "Multiplica" -> 0
+                       "Subtrai" -> -num
+                       "Divide" -> 0
+operador ((cmd,num):pairs)
+ | cmd == "Divide" && num == 0 = -666
+ | (operador (pairs)) == -666   = -666
+ | otherwise                   = case cmd of
+                                 "Soma" -> operador (pairs) + num
+                                 "Multiplica" -> operador (pairs) * num
+                                 "Subtrai" -> operador (pairs) - num
+                                 "Divide" -> operador (pairs) `div` num
+
 executa :: [(Comando,Valor)] -> Int
 executa [] = 0
-executa [(cmd,num)] = case cmd of
-                      "Soma" -> num
-                      "Multiplica" -> 0
-                      "Subtrai" -> -num
-                      "Divide" -> 0
-executa ((cmd,num):pairs) 
- | cmd == "Divide" && num == 0 = -666
- | (executa (pairs)) == -666   = -666
- | otherwise                   = case cmd of
-                                 "Soma" -> num + (executa (pairs))
-                                 "Multiplica" -> num * (executa (pairs))
-                                 "Subtrai" -> num - (executa (pairs))
-                                 "Divide" -> num `div` (executa (pairs))
+executa instruc = operador (invertPairs instruc)
 
 main = do
     a <- getLine

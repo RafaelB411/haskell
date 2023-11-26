d50deb4539b088aab8a2419c6c9b69137a71a690
RafaelB411 <rafaelb411.rb@gmail.com>
2023-11-26 15:23:45 -0300

    question 7 done

--- lista-1/FaturaCartao.hs
+++ lista-1/FaturaCartao.hs
@@ -1,4 +1,6 @@
 split :: Char -> String -> [String]
+-- split a string into substrings when of occurrence 
+-- of a given char
 split _ [] = []
 split key str = let (seg,str') = break (== key) str
                  in seg : case str' of
@@ -6,17 +8,21 @@ split key str = let (seg,str') = break (== key) str
                           (_:str'') -> split key str''
 
 listMonthValues :: [String] -> [(String,Double)]
+-- sort strings from given array on tuples of string/double
+-- matching months and corresponding values 
 listMonthValues [] = []
 listMonthValues (a:b:c:segs) = let (_,a') = break (== ' ') a 
                               in case a' of
                                  (_:a'') -> (a'',read c) : listMonthValues segs
 
 filterValues :: String -> [(String,Double)] -> [Double]
+-- select values according to a given month
 filterValues _ [] = []
 filterValues key ((month,val):tuples) | month == key  = val : filterValues key tuples
                                       | otherwise     = filterValues key tuples
 
 logMes :: String -> String -> Double
+-- return the total value spent on a given month
 logMes key str = foldl (+) 0 (filterValues key (listMonthValues (split ';' str)))
 
 main = do
